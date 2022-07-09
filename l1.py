44a3400a1b626f498caad888a480477fe27fe141
rudykas <108556842+rudykas@users.noreply.github.com>
2022-07-09 16:39:53 +0300

    Add files via upload

---
+++ l1.py
@@ -0,0 +1,20 @@
+from turtle import*
+speed('fastest')
+
+def square(side):
+    for x in range(4):
+        forward(side)
+        left(90)
+        
+square(100)
+
+#for y in range(360):
+ #   forward(y)
+  #  backward(y)
+   # left(1)
+
+# def line(length):
+  #  for x in range(2):
+   #     forward(length)
+    #    left(180)
+
